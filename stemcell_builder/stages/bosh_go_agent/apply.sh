c5bf62e9cef61c914136e19e8b1fa0265107038e
Tanzeeb Khalili <tkhalili@pivotal.io>
2017-09-14 12:01:35 -0400

    Update to the latest bosh-agent-tls
    [#150974136](https://www.pivotaltracker.com/story/show/150974136)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -30,7 +30,7 @@ if is_ppc64le; then
   echo "d034db7caa859ad64ea35bd262ac87ea9c24c21aed1f30c4f749b49bfa11aa96  bosh-agent" | shasum -a 256 -c -
 else
   curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-toronto-bag/bosh-agent-tls"
-  echo "00e5628dd83bd0099e66247ff50e578cfb611cab6df92b5510151003dee403e9  bosh-agent" | shasum -a 256 -c -
+  echo "62727b42b7473e53927f67bb5de03abff2ab07440a416aebebe7bc1b9a2c9f66  bosh-agent" | shasum -a 256 -c -
 fi
 mv bosh-agent $chroot/var/vcap/bosh/bin/
 
